8d23b256c98862b59d8b38688731b7febd68fcf4
Aaron Robinett <AaronRobinett@provalus.com>
2019-03-29 11:49:23 -0500

    example algorithms added @ 03-29-2019_11:49:23

---
+++ sort-by-low/fileparse1.py
@@ -0,0 +1,42 @@
+# working with file parsing
+import time
+
+start = time.time()
+with open("example.txt", "r") as myfile:
+	#print(myfile)
+	innerstart = time.time()
+	mylist = []	
+	for line in myfile.readlines():
+		entries = line.split() # split file by line
+		if len(entries) != 0: # if not EOF, split line by comma
+			items = entries[0].split(",")
+			try:	# convert string in third index to integer
+				items[2] = int(items[2])
+			except Exception as err: # sloppy error catching
+				print("Error: ",err)
+			else:
+				print("line items for this iteration:",items)
+			try:	
+				""" 
+				if mylist is empty and items[2] is an integer, 
+				populate mylist with the interfaceID and throughput value from items
+				(2nd and 3rd index respectively)
+				
+				Compare current value in items[2] with integer in mylist.
+				If items[2] is lower, overwrite contents of mylist with 
+				new interfaceID and throughput value
+				"""
+				if len(mylist) == 0 and isinstance(items[2], int):
+					mylist.append((items[1],items[2]))
+
+				elif len(mylist) != 0 and items[2] < mylist[0][1]:
+					mylist[0] = (items[1],items[2])
+			except Exception as err:
+				print("Error: ",err)
+			finally:
+				print("what gets stored:",mylist)
+	innerend = time.time()
+	print("Inner time: ",innerend - innerstart)
+
+end = time.time()
+print("Inner time + file IO: ",end - start)
---
+++ sort-by-low/fileparse2.py
@@ -0,0 +1,47 @@
+# working with file parsing
+import time
+import collections
+
+start = time.time()
+with open("example.txt", "r") as myfile:
+	#print(myfile)
+	innerstart = time.time()
+	mydeque = collections.deque()
+	for line in myfile.readlines():
+		entries = line.split() # split file by line
+		if len(entries) != 0: # if not EOF, split line by comma
+			items = entries[0].split(",")
+			try:	# convert string in third index to integer
+				items[2] = int(items[2])
+			except Exception as err: # sloppy error catching
+				print("Error: ",err)
+			else:
+				print("line items for this iteration:",items)
+			try:	
+				""" 
+				if mydeque is not empty and items[2] is less than the value stored in mydeque, 
+				prepend mydeque with the interfaceID and throughput value from items
+				(index 1 and 2 of items, respectively)
+
+				if mydeque is empty or items[2] is greater than what is already there, 
+				append the values from items instead. 
+				
+				This should both populate the first valid entry
+				AND 
+				ensure that the lowest throughput value always exists index 0 of mydeque
+				"""
+				if isinstance(items[2],int):					
+					if len(mydeque) != 0 and items[2] < mydeque[0][1]:
+						mydeque.appendleft((items[1],items[2]))
+					else:
+						mydeque.append((items[1],items[2]))
+
+			except Exception as err:
+				print("Error: ",err)
+			finally:
+				print("what gets stored:",mydeque)
+	innerend = time.time()
+	print("Inner time: ",innerend - innerstart)
+
+end = time.time()
+print("Inner time + file IO: ",end - start)
